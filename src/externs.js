585c73e690797241cea63edd6642712e8a0722c7
Koichi Suematsu <pascal.jp@gmail.com>
2014-11-26 01:28:49 +0900

    Fix external.js so that compiler won't rename function names.

--- src/externs.js
+++ src/externs.js
@@ -1,9 +1,34 @@
+// https://developer.chrome.com/apps/bluetooth
 chrome.bluetooth = function() {};
 chrome.bluetooth.getAdapterState = function(callback) {};
 chrome.bluetooth.getDevice = function(deviceAddress, callback) {};
 chrome.bluetooth.getDevices = function(callback) {};
 chrome.bluetooth.startDiscovery = function(callback) {};
 chrome.bluetooth.stopDiscovery = function(callback) {};
+
+// https://developer.chrome.com/apps/bluetoothSocket
+chrome.bluetoothSocket = function() {};
+chrome.bluetoothSocket.create = function(properties, callback) {};
+chrome.bluetoothSocket.update = function(socketId, properties, callback) {};
+chrome.bluetoothSocket.setPaused = function(socketId, paused, callback) {};
+chrome.bluetoothSocket.listenUsingRfcomm = function(socketId, uuid, options, callback) {};
+chrome.bluetoothSocket.listenUsingL2cap = function(socketId, uuid, options, callback) {};
+chrome.bluetoothSocket.connect = function(socketId, address, uuid, callback) {};
+chrome.bluetoothSocket.disconnect = function(socketId, callback) {};
+chrome.bluetoothSocket.close = function(socketId, callback) {};
+chrome.bluetoothSocket.send = function(socketId, data, callback) {};
+chrome.bluetoothSocket.getInfo = function(socketId, callback) {};
+chrome.bluetoothSocket.getSockets = function(callback) {};
+chrome.bluetoothSocket.onAccept = {};
+chrome.bluetoothSocket.onAccept.addListener = function(callback) {};
+chrome.bluetoothSocket.onAcceptError = {};
+chrome.bluetoothSocket.onAcceptError.addListener = function(callback) {};
+chrome.bluetoothSocket.onReceive = {};
+chrome.bluetoothSocket.onReceive.addListener = function(callback) {};
+chrome.bluetoothSocket.onReceiveError = {};
+chrome.bluetoothSocket.onReceiveError.addListener = function(callback) {};
+
+// https://developer.chrome.com/apps/fileSystem
 chrome.fileSystem.chooseEntry = function(options, callback) {};
 chrome.fileSystem.getDisplayPath = function(entry, callback) {};
 chrome.fileSystem.getWritableEntry = function(entry, callback) {};
@@ -11,23 +36,11 @@ chrome.fileSystem.isWritableEntry = function(entry, callback) {};
 chrome.fileSystem.restoreEntry = function(id, callback) {};
 chrome.fileSystem.isRestorable = function(id, callback) {};
 chrome.fileSystem.retainEntry = function(entry) {};
-chrome.runtime = function() {};
 
-// https://developer.chrome.com/apps/bluetoothSocket
-var BluetoothSocket = function() {};
-chrome.bluetoothSocket.update = function(socketId, properties, callback) {};
-chrome.bluetoothSocket.setPaused = function(socketId, paused, callback) {};
-chrome.bluetoothSocket.listenUsingRfcomm = function(socketId, uuid, options, callback) {};
-chrome.bluetoothSocket.listenUsingL2cap = function(socketId, uuid, options, callback) {};
-chrome.bluetoothSocket.disconnect = function(socketId, callback) {};
-chrome.bluetoothSocket.send = function(socketId, data, callback) {};
-chrome.bluetoothSocket.getInfo = function(socketId, callback) {};
-chrome.bluetoothSocket.getSockets = function(callback) {};
-chrome.bluetoothSocket.close = function(socketId, callback) {};
-chrome.bluetoothSocket.connect = function(socketId, address, uuid, callback) {};
-chrome.bluetoothSocket.create = function(properties, callback) {};
-chrome.bluetoothSocket.onReceive = {};
-chrome.bluetoothSocket.onReceive.addListener = function(callback) {};
+// https://developer.chrome.com/apps/runtime
+chrome.runtime = function() {};
+chrome.runtime.lastError = {};
+chrome.runtime.lastError.message = null;
 
 // https://developer.chrome.com/apps/bluetoothSocket#type-SocketProperties
 var SocketProperties = function() {};
@@ -44,7 +57,7 @@ ListenOptions.prototype.backlog = 0;
 // https://developer.chrome.com/apps/bluetoothSocket#type-SocketInfo
 var SocketInfo = function() {};
 SocketInfo.prototype.socketId = 0;
-SocketInfo.prototype.socketId = false;
+SocketInfo.prototype.persistent = false;
 SocketInfo.prototype.name = null;
 SocketInfo.prototype.bufferSize = 0;
 SocketInfo.prototype.paused = false;
@@ -73,5 +86,3 @@ Device.prototype.type = null;
 Device.prototype.paired = false;
 Device.prototype.connected = false;
 Device.prototype.uuids = [];
-
-chrome.bluetoothSocket = function() {};
